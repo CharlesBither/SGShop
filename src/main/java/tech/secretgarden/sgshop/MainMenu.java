9579bff0f9c340f99f2a08523f27b342a3cfdb9b
Charles Bither <95773999+CharlesBither@users.noreply.github.com>
2023-08-29 19:05:18 -0700

    add basic classes

---
+++ src/main/java/tech/secretgarden/sgshop/MainMenu.java
@@ -0,0 +1,12 @@
+package tech.secretgarden.sgshop;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+public class MainMenu {
+
+    public Inventory getMainMenu() {
+        //Inventory menu = Bukkit.createInventory(null, )
+        return null;
+    }
+}
--- src/main/java/tech/secretgarden/sgshop/SGShop.java
+++ src/main/java/tech/secretgarden/sgshop/SGShop.java
@@ -2,16 +2,51 @@ package tech.secretgarden.sgshop;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public final class SGShop extends JavaPlugin {
+
+    Database database = new Database();
+
+
+    public static ArrayList<String> dbList = new ArrayList<>();
+    public ArrayList<String> getDbList() {
+        dbList.add(getConfig().getString("HOST"));
+        dbList.add(getConfig().getString("PORT"));
+        dbList.add(getConfig().getString("DATABASE"));
+        dbList.add(getConfig().getString("USERNAME"));
+        dbList.add(getConfig().getString("PASSWORD"));
+        return dbList;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        if (getConfig().getString("HOST") != null) {
+            try {
+                getDbList();
+                database.connect();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Connected to database = " + database.isConnected());
+        System.out.println("SGShop plugin has loaded");
+
+        getCommand("shop").setExecutor(new ShopCommand());
+
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("SGShop has unloaded");
+        database.disconnect();
     }
 }
